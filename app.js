5aa20baaef5a7078e87b0ebd0b21a851261d5079
Allan Andrade <allanandrade.empreendimentos@gmail.com>
2022-11-21 09:55:00 -0300

    Adding innovation section

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const controller = new ScrollMagic.Controller();
 
 
 // Scene
-const scene = new ScrollMagic.Scene({
+const hero_scene = new ScrollMagic.Scene({
     duration: 4500,
     triggerElement: hero,
     triggerHook: 0,
@@ -19,7 +19,7 @@ let accelAmount = 0.1;
 let scrollPos = 0;
 let delay = 0;
 
-scene.on("update", e => {
+hero_scene.on("update", e => {
     scrollPos = e.scrollPos / 500;
 })
 
@@ -28,3 +28,23 @@ setInterval(() => {
 
     intro_video.currentTime = delay;
 }, 40)
+
+
+let timeline = new TimelineMax().add(
+    [
+        TweenMax.to('h2', {'clip-path': 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%', opacity: 1, y:0, duration: 2.2})
+    ]
+)
+
+
+const inovation = document.querySelector('.inovation');
+
+
+const inovation_scene = new ScrollMagic.Scene({
+    duration: 1000,
+    triggerElement: inovation,
+})
+.setPin(inovation)
+.setTween(timeline)
+.addIndicators()
+.addTo(controller)
